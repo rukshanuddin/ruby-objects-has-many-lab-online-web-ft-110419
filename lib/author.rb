2db77448117389bc10bea1433098085ccdb9b844
Rukshan Uddin <rukshan.uddin@gmail.com>
2019-11-14 00:55:39 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -2,13 +2,13 @@ class Author
 
   attr_accessor :name, :post
 
-  @@posts = []
+  
   def initialize(name)
     @name = name
   end
 
   def posts
-
+    @posts = Post.all.find_all { |post| post.author == self  }
   end
 
   def add_post(post)
